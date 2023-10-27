fa9d261012da7f6e76ec3e29e58e84c1c9fa788e
Diana de la O <delaoitzel17@gmail.com>
2023-10-27 14:59:04 -0600

    sistema de procesos

--- vis/home.php
+++ vis/home.php
@@ -38,38 +38,73 @@ if($varsesion== null || $varsesion=''){
             </nav>
         </div>
     </section>
-    <section class="inputs-section">
-        <div class="container-inputs">
-            <form action="" method="">
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Puesto</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Direccion</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp">
-                </div>
-            </form>
-            <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Proceso</label>
-                      <select class="form-select form-control">
-                        <option selected disabled>Selecciona una opción</option>
-                        <?php
-                            include ("../../config/conexion.php");
-
-                            $sql = $conexion->query("SELECT * FROM procesos");
-                            while ($resultado = $sql->fetch_assoc()) {
-                            echo "<option value='".$resultado['Nom_Procesos']."'>".$resultado['Nom_Procesos']."</option>";
-                          }
-                        ?>
-                      </select>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-5">
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label">Nombre</label>
+                        <input type="text" class="form-control">
                     </div>
+                    <div class="mb-3">
+                        <label class="form-label">Puesto</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Direccion</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nombre Subproceso</label>
+                        <select class="form-select mb-3" name="nom_subproceso">
+                        <option selected disabled>Selecciona una opción</option>
+                            <?php
+                                include ("../config/conexion.php");
+    
+                                $sql = $conexion->query("SELECT * FROM sub_proceso");
+                                while ($resultado = $sql->fetch_assoc()) {
+                                echo "<option value='".$resultado['nom_Subproceso']."'>".$resultado['nom_Subproceso']."</option>";
+                              }
+                            ?>
+                        </select>
+                    </div>
+                </form>
+            </div>
+            <div class="col-7">
+                <div class="card" style="width: 65rem;">
+                    <div class="card-body">
+                        <table class="table">
+                            <thead class="text-center">
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Funciones</th>
+                                    <th scope="col">Procesos</th>
+                                    <th scope="col">Sub_procesos</th>
+                                    <th scope="col">Actividades</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                <?php
+                                    $query = "SELECT * FROM consultas";
+                                    $result_task = mysqli_query($conexion, $query);
+
+                                    while($row = mysqli_fetch_assoc($result_task)) { ?>
+                                    <tr>
+                                        <td><?php echo $row['ID']; ?></td>
+                                        <td><?php echo $row['Nom_funcion']; ?></td>
+                                        <td><?php echo $row['nom_Procesos']; ?></td>
+                                        <td><?php echo $row['nom_subproceso']; ?></td>
+                                        <td><?php echo $row['nom_Actividad']; ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
+
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
